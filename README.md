953a207a9a96c56d26e081bae4253cc674a76953
Teemu Salonen <teemutapani.salonen@gmail.com>
2026-01-17 17:43:22 +0200

    Project startup and initial database design

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # database-systems-nosql-project
-Repository for the TUNI course DATA.DB.300 – Database Systems: NoSQL project
+
+Repository for the TUNI course DATA.DB.300 – Database Systems: NoSQL project.
+
+## Setup
+
+### Prerequisites
+
+- Docker
+- Docker compose
+- A database management tool can be helpful
+
+### Running the project
+
+1. Clone the repository:
+```
+git clone https://github.com/SalonenTeemu/database-systems-nosql-project.git
+```
+
+2. Navigate to the project directory:
+```
+cd database-systems-nosql-project
+```
+
+3. Start the applciation using Docker compose:
+```
+docker-compose up
+```
+
+## Documentation
+
+The `documentation/` folder contains:
+- The project report
+- Database design and architecture documents
+
+## SQL code 
+
+The `sql/` folder contains:
+- Table creation statements
+- Data insertion scripts
+- Queries for the project use cases
---
+++ sql/ddl/schema.sql
@@ -0,0 +1,56 @@
+-- User table to store user information
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Publisher table to store game publisher information
+CREATE TABLE publishers (
+    publisher_id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Game table to store game information
+CREATE TABLE games (
+    game_id SERIAL PRIMARY KEY,
+    publisher_id INT NOT NULL REFERENCES publishers(publisher_id),
+    title TEXT UNIQUE NOT NULL,
+    description TEXT,
+    price NUMERIC(8,2) NOT NULL CHECK (price >= 0),
+    release_date DATE,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Genre table to store game genres
+CREATE TABLE genres (
+    genre_id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+-- Junction table to associate games with genres (many-to-many relationship)
+CREATE TABLE game_genres (
+    game_id INT REFERENCES games(game_id),
+    genre_id INT REFERENCES genres(genre_id),
+    PRIMARY KEY (game_id, genre_id)
+);
+
+-- Purchase table to store data about user game purchases
+CREATE TABLE purchases (
+    purchase_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(user_id),
+    total_price NUMERIC(8,2) NOT NULL,
+    purchase_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Junction table to associate users with their purchased games
+CREATE TABLE user_games (
+    user_id INT REFERENCES users(user_id),
+    game_id INT REFERENCES games(game_id),
+    purchase_id INT REFERENCES purchases(purchase_id),
+    PRIMARY KEY (user_id, game_id)
+);
